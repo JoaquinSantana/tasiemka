7b0dfaebc76086ba237353886a44bffceb73bc46
Tomasz Panek <joachimsantana@gmail.com>
2015-09-11 12:02:28 +0200

    Remove bin from linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, '/home/panczo/tasiemka'
 # set :pty, true
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 # Default value for :linked_files is []1
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
