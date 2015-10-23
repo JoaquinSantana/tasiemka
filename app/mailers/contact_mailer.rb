1efd6c332a896ba2f0b459cbcbd837e4d777ea17
Tomasz Panek <joachimsantana@gmail.com>
2015-10-23 14:27:06 +0200

    Now tasiemka can send emails from users

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,17 @@
+class ContactMailer < ActionMailer::Base
+  default to: "informacje@tasiemka.info"
+
+  def new_contact(contact)
+    @contact = contact
+    
+    if @contact.subscribe?
+      @email = @contact.email
+    elsif !@contact.subscribe? && !@contact.email.blank?
+      @email = @contact.email
+    else
+      @email = "Anonim"
+    end
+
+    mail(from: @email, subject: "#{contact.wiadomosc}")
+  end
+end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -12,4 +12,12 @@
 #
 
 class Contact < ActiveRecord::Base
+  validates :wiadomosc, presence: true, length: { minimum: 10, :message => "Wiadomość musi zawierać 10 znaków" }
+  validates :email, presence: true, unless: Proc.new { |a| a.subscribe.blank? }
+
+  after_create :new_contact
+
+  def new_contact
+    ContactMailer.new_contact(self).deliver_now
+  end
 end
