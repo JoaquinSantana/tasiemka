d0ea7ba21745555f813f7203a21bece52261ee75
Tomasz Panek <joachimsantana@gmail.com>
2016-02-22 13:00:33 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -75,4 +75,19 @@ ActiveRecord::Schema.define(version: 20160217122249) do
 
   add_index "sites", ["name"], name: "index_sites_on_name"
 
+  create_table "votes", force: :cascade do |t|
+    t.integer  "votable_id"
+    t.string   "votable_type"
+    t.integer  "voter_id"
+    t.string   "voter_type"
+    t.boolean  "vote_flag"
+    t.string   "vote_scope"
+    t.integer  "vote_weight"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
+
 end
