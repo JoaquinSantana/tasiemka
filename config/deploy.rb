e426e21f08c1f946c85c64bfa9e2d31f7ac31c35
Tomasz Panek <joachimsantana@gmail.com>
2016-02-23 11:31:57 +0100

    remove sample files

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :deploy_to, '/home/panczo/tasiemka'
 # Default value for :pty is false
 # set :pty, true
 
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 # Default value for :linked_files is []1
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
